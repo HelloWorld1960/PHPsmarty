6bd2db72c0aa269e819d910297fd0a1eaa68aa3b
HelloWorld1960 <duzter_jaguar@hotmail.com>
2019-08-20 19:57:43 -0500

    Add comments to the code.

--- 25MD5Sha/index.php
+++ 25MD5Sha/index.php
@@ -1,8 +1,10 @@
 <?php
-
+/*sha1 — Calcula el 'hash' sha1 de un string utilizando el Algoritmo 1 US Secure Hash Algorithm 1.*/
 $sha = sha1('12345');
+/*md5 — Calcula el 'hash' md5 de un string utilizando el Algoritmo MD5 Message-Digest de RSA Data Security, Inc.*/
 $md5 = md5('12345');
 
+/*strlen — Obtiene la longitud de un string*/
 echo "sha1= ",$sha," y tiene ",strlen($sha), " caracteres<br/><br/>";
 echo "MD5= ",$md5," y tiene ",strlen($md5), " caracteres<br/><br/>";
 
