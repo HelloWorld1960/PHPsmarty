4e1ada69f7d5ea83a654a0f725b45220333eed87
HelloWorld1960 <duzter_jaguar@hotmail.com>
2019-08-20 19:52:30 -0500

    Upload MD5Sha project.

---
+++ 25MD5Sha/index.php
@@ -0,0 +1,9 @@
+<?php
+
+$sha = sha1('12345');
+$md5 = md5('12345');
+
+echo "sha1= ",$sha," y tiene ",strlen($sha), " caracteres<br/><br/>";
+echo "MD5= ",$md5," y tiene ",strlen($md5), " caracteres<br/><br/>";
+
+?>
